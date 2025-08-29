b9a8375490a761aa1c17ab842778e9677e3cc3fc
Robert <robert11122002@gmail.com>
2025-08-29 11:12:17 +0100

    Update example

--- examples/main.cc
+++ examples/main.cc
@@ -1,3 +1,17 @@
-#include "dynamicxx/dynamicxx.h"
+#include <dynamicxx/dynamicxx.h>
 
-int main() {}
+#include <iostream>
+
+using dynamicxx::Dynamic;
+
+int main() {
+    Dynamic d;
+
+    d = 123;  // It will figure out which type to construct.
+
+    d = 42.0F;  // It will figure out which type to create.
+    std::cout << "Float value: " << d.GetNumber() << '\n';
+
+    d = "Hello world";  // It will figure it out again.
+    std::cout << "String value: " << d.GetString() << '\n';
+}
