65a20e5e5b35b319b8001898a1755b6b27e9aa65
Robert <robert11122002@gmail.com>
2025-08-29 11:13:08 +0100

    Basic test

--- tests/main.cc
+++ tests/main.cc
@@ -1,5 +1,5 @@
-#include "dynamicxx/dynamicxx.h"
-#include "gtest/gtest.h"
+#include <dynamicxx/dynamicxx.h>
+#include <gtest/gtest.h>
 
 using dynamicxx::Dynamic;
 
@@ -7,7 +7,10 @@ TEST(DynamicTest, BasicDynamicText) {
     static constexpr auto StringToUse = "Foobar";
     Dynamic d = Dynamic::From<Dynamic::String>(StringToUse);
 
+    std::string s = d;
+
     EXPECT_EQ(d.GetString(), StringToUse);
+    EXPECT_EQ(d.GetString(), s);
 }
 
 TEST(DynamicTest, BasicTest) {}
