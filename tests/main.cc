dd757e2eb8eaa155972f4fc4721aad4b0b7c20f6
Robert <robert11122002@gmail.com>
2025-08-29 22:46:08 +0100

    Test copying

--- tests/main.cc
+++ tests/main.cc
@@ -1,6 +1,8 @@
 #include <dynamicxx/dynamicxx.h>
 #include <gtest/gtest.h>
 
+#include <cstddef>
+
 using dynamicxx::Dynamic;
 
 TEST(DynamicTest, BasicDynamicText) {
@@ -15,10 +17,37 @@ TEST(DynamicTest, BasicDynamicText) {
 
 TEST(DynamicTest, BasicTest) {
     static constexpr auto FooBarKey = "Foobar";
+    static constexpr auto FooBarValue = 42;
 
     Dynamic d = Dynamic::From<Dynamic::Object>();
-    d[FooBarKey] = 42;
+    d[FooBarKey] = FooBarValue;
 
     EXPECT_TRUE(d.Contains(FooBarKey));
-    EXPECT_EQ(d[FooBarKey], 42);
+    EXPECT_EQ(d[FooBarKey], FooBarValue);
+}
+
+TEST(DynamicTest, CopyConstructionAndAssignment) {
+    static constexpr std::size_t Size = 12;
+
+    Dynamic d = Dynamic::From<Dynamic::Array>(Size);
+
+    constexpr auto PerformAsserts = [](const Dynamic& d) {
+        EXPECT_EQ(d.GetArray().size(), Size);
+        for (const auto& value : d.GetArray()) {
+            EXPECT_TRUE(value.IsUndefined());
+        }
+    };
+    PerformAsserts(d);
+
+    const auto copy = d;
+    PerformAsserts(copy);  // Copy should be the same
+    PerformAsserts(d);     // Original should also be like this
+
+    d.Push(42);
+    d.Push("Hello, world");
+    d[1] = Dynamic::Array{};
+    d[2] = 123;
+
+    const auto another_copy = d;
+    ASSERT_EQ(d, another_copy);  // They should be deeply equal
 }
